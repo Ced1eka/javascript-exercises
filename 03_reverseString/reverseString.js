2e045200baec282a65dd8fbcdd881fbe0ab9ec8d
Ced <cedieka2@gmail.com>
2023-09-08 04:33:42 +0900

    Finished exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+      let strings = word.toString();
+      const splits = strings.split('');
+      const reversed = splits.reverse();
+      const joined = reversed.join('');
+      // const result = joined.toString();
+      return joined;
 };
 
 // Do not edit below this line
