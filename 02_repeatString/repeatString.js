bd7b434fe1889bafcb2e0ff3a6761e9924b4fb39
Ced <cedieka2@gmail.com>
2023-09-08 03:09:09 +0900

    Modified and finished exercises 01 and 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,19 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+      let number = 0;
+      if (num === number){
+            return('')
+      }else if (num > number){
+            let str = '';
+            if (string === ''){
+                  return('')
+            }
+            for (let i = 0; i < num; i++){
+                  str += string;
+            }
+            return str;
+      }else if (num < number){
+            return ('ERROR');
+      }
 
 };
 
