4e8601d74073953cb17f327efdb90b03b7a5f513
Ced <cedieka2@gmail.com>
2023-09-09 04:41:47 +0900

    Finally finished exercise 04
    Crying hard rn on how much of a brainlet I am.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...values) {
+
+      let hotNewArr = [];
+
+      for (let i = 0; i < array.length; i++){
+
+            if (!values.includes(array[i])) {
+                  hotNewArr.push(array[i]);
+                }
+      }
+return hotNewArr;
 
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
