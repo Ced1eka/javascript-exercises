208d1bef438d8aa7bdd3bb251d347aaa1e62591d
Ced <cedieka2@gmail.com>
2023-09-10 05:45:50 +0900

    Finished exercise 08.
    This went a little bit easier than what i expected.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(add1,add2) {
+	return Number(add1) + Number(add2);
 };
 
-const subtract = function() {
-	
+const subtract = function(sub1, sub2) {
+  return sub1 - sub2;
 };
 
-const sum = function() {
-	
+const sum = function(Arr) {
+  let result = 0;
+  Arr.forEach((num) => {return result += num;})
+  return result;
+
 };
 
-const multiply = function() {
+const multiply = function(Arr) {
+
+  return Arr.map(Number).reduce((accumulator, currentValue) => {return accumulator * currentValue});
 
 };
 
-const power = function() {
-	
+const power = function(num,pwr) {
+
+  return Math.pow(num,pwr)
 };
 
-const factorial = function() {
-	
+const factorial = function(fctr) {
+
+
+if (fctr === 0 || fctr === 1) return 1;
+
+for (let i = fctr -1; i>=1;i-- ) {fctr*=i;}
+
+return fctr;
+
 };
 
 // Do not edit below this line
