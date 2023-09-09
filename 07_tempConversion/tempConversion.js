0e21f7761db72965543c1f2034f8e7abd0f0d180
Ced <cedieka2@gmail.com>
2023-09-10 04:37:18 +0900

    Finished the 07th exercise.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,21 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(Cel) {
+ return Math.round(((Cel - 32) / 1.800) * 10) / 10;
+
+
+
+  // if (Cel >= 0) {
+  //   let result = Math.round((Cel - 32) / 1.800);
+  //   alert(result)
+  // } else if (Cel > 100){
+  //   let result =  Number(((Cel - 32) / 1.800).toFixed(1));
+  //   alert(result)
+  // }
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(Cel) {
+ return Math.round(((Cel * 1.8000) + 32) * 10) / 10;
+
 };
 
 // Do not edit below this line
