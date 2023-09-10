7b18f924b01c6835a795dc818aa6a6d322901401
Ced <cedieka2@gmail.com>
2023-09-11 01:07:00 +0900

    Finished this exercise faster this time.
    I'm trying to not overly fixate on things and just
    do my best and move on.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(param) {
+let num;
+let paramN = Number(param);
+let fib = [0,1];
+
+if (paramN < 0) return('OOPS');
+if (paramN === 1) return(1);
+
+for (let i = 2; i <= paramN; i++){
+      fib[i] = fib[i-2] + fib[i-1];
+      num = fib[i];
+}
+return num;
 
 };
 
