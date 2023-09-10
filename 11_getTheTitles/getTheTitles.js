a3c7912b3b1dd59374a27bb5f7524663275e8732
Ced <cedieka2@gmail.com>
2023-09-11 01:32:25 +0900

    Finished exercise 11!

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,21 @@
 const getTheTitles = function() {
 
+      const books = [
+            {
+              title: 'Book',
+              author: 'Name'
+            },
+            {
+              title: 'Book2',
+              author: 'Name2'
+            }
+          ]
+
+
+      let maps = books.map(book => book.title);
+      return maps;
+
+
 };
 
 // Do not edit below this line
